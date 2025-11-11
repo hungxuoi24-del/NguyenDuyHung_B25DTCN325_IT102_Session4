e6a7312e87707c9273468ffa5276acc19e4c5741
Nguyễn Duy Hưng <hungxuoi24@gmail.com>
2025-11-12 01:34:14 +0700

    Update bai 2.c

--- bai 2.c
+++ bai 2.c
@@ -4,19 +4,11 @@ int main(){
 	int n;
 	printf("Nhap vao mot so nguyen: ");
 	scanf("%d", &n);
-	if(n % 3 == 0 && n % 5 ==0){
-		printf("So vua nhap chia het cho ca 3 va 5.");
+	if(n % 2 == 0){
+		printf("So vua nhap la so chan. ");
+	}else{
+		printf("So vua nhap la so le.");
 	}
-	else if(n % 3 == 0){
-		printf("So vua nhap chia het cho 3.\n");
-	}
-	else if(n % 5 ==0){
-		printf("So vua nhap chia het cho 5.\n");
-	}
-	else{
-		printf("So vua nhap la so 0.\n");
-	}
-
 	
 	return 0;
 }
